c5340325c46f41df1b31a8544d8806ff7ff91442
shantanuj <shantanu12jswl@gmail.com>
2016-09-07 12:23:32 +0800

    Adding all files

---
+++ js/extensionscroll.js
@@ -0,0 +1,21 @@
+/*Smooth Scroll up/down ; Author:http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links/*/
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+
+
